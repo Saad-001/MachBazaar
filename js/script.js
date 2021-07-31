10b4a35bc2438f469a977aafa6d85c7a4e6107d9
Saad-001 <saadafnan21@gmail.com>
2021-07-31 21:27:41 +0600

    some updates

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$('.owl-carousel').owlCarousel({
+  loop: false,
+  margin: 10,
+  nav:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      769:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+
+
+// grab everything we need
+// const btn = document.querySelector(".mobile-menu-button");
+// const sidebar = document.querySelector(".sidebar");
+
+// // add our event listener for the click
+// btn.addEventListener("click", () => {
+//   sidebar.classList.toggle("-translate-x-full");
+//   sidebar.classList.add("z-10");
+// });
